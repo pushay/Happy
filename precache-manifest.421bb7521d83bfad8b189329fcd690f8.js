539b4f2cc8193a029fc31f0e181050612118100b
pushay <sobocinskapraca@gmail.com>
2020-11-03 19:45:48 +0100

    Updates

---
+++ precache-manifest.421bb7521d83bfad8b189329fcd690f8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99f8e333e8c56fa035a3e5793ef398b",
+    "url": "/Happy/index.html"
+  },
+  {
+    "revision": "f49c4f7884dd59b3d9a3",
+    "url": "/Happy/static/css/main.6847cb8b.chunk.css"
+  },
+  {
+    "revision": "7223b931cf6885e11345",
+    "url": "/Happy/static/js/2.616a2567.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/Happy/static/js/2.616a2567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49c4f7884dd59b3d9a3",
+    "url": "/Happy/static/js/main.3c4d1a48.chunk.js"
+  },
+  {
+    "revision": "fc3333a571b32952f9f7",
+    "url": "/Happy/static/js/runtime-main.94b4942c.js"
+  },
+  {
+    "revision": "3c1789bee106a822d8655c4c1783ee26",
+    "url": "/Happy/static/media/clouds.3c1789be.jpg"
+  },
+  {
+    "revision": "00a296ea5795ac89de7f0b1049d981d9",
+    "url": "/Happy/static/media/inspired.00a296ea.jpg"
+  },
+  {
+    "revision": "78902b54dd0f83fbb483e68d0ff4aa32",
+    "url": "/Happy/static/media/meditate.78902b54.jpg"
+  },
+  {
+    "revision": "42c22ec63e1650488a7acd13b3ef4727",
+    "url": "/Happy/static/media/night.42c22ec6.jpg"
+  },
+  {
+    "revision": "4ad03eb9d13a8eda0bd41286a901d201",
+    "url": "/Happy/static/media/night.4ad03eb9.mp3"
+  },
+  {
+    "revision": "dc562961622f863f482c9e6502bef3b6",
+    "url": "/Happy/static/media/notebook.dc562961.jpg"
+  },
+  {
+    "revision": "8f2ec4e96bc63a021ad8ace34768384a",
+    "url": "/Happy/static/media/rain.8f2ec4e9.mp3"
+  },
+  {
+    "revision": "13e8ceff4e8a9016f88e157c5f2353c6",
+    "url": "/Happy/static/media/raining.13e8ceff.jpg"
+  },
+  {
+    "revision": "53794f0da795c60fa76786b5d89c45ab",
+    "url": "/Happy/static/media/relax.53794f0d.mp3"
+  },
+  {
+    "revision": "929b543d7ddd37c280814304fa073f07",
+    "url": "/Happy/static/media/relax.929b543d.jpg"
+  },
+  {
+    "revision": "ee85739a57efe0ac2d3535d9dbb4edab",
+    "url": "/Happy/static/media/sea.ee85739a.mp3"
+  },
+  {
+    "revision": "1f64efb3e5350cec4250ee07c2de8a07",
+    "url": "/Happy/static/media/sky.1f64efb3.jpg"
+  },
+  {
+    "revision": "10cffa76aaed926b417a1d8502efbcb3",
+    "url": "/Happy/static/media/soap.10cffa76.mp3"
+  },
+  {
+    "revision": "835245605763b5851187b116aff45f7e",
+    "url": "/Happy/static/media/soap.83524560.jpg"
+  },
+  {
+    "revision": "c90fe0bb292636ccbaa91adf7d5963e1",
+    "url": "/Happy/static/media/waves.c90fe0bb.jpg"
+  }
+]);
