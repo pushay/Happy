46d756975e3bc58e0d935eb999d49b9198c13cc1
pushay <sobocinskapraca@gmail.com>
2020-11-03 19:38:11 +0100

    Updates

--- src/store/actions.jsx
+++
@@ -1,5 +0,0 @@
-export const POST_DATA = 'POST_DATA';
-export const GET_DATA = 'GET_DATA';
-export const LOG_IN = 'LOG_IN';
-export const LOG_OFF = 'LOG__OFF'
-export const INIT_FIREBASE = 'INIT_FIREBASE';
